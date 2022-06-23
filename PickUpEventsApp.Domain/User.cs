c4611f42a7619c3117978761f36f9874b28e376e
Andrea <andreaschwenk3@gmail.com>
2022-06-22 19:49:35 -0500

    created the User entity- with commenetd out properties which are going to be created elsewhere

---
+++ PickUpEventsApp.Domain/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickUpEventsApp.Domain
+{
+    internal class User
+    {
+      string Name { get; set; }
+      string EmailAddress { get; set; }
+      string PhoneNumber { get; set; }
+      // Location Location { get; set; }
+      //IEnumerable<Group> Groups { get; set; }
+      //IEnumerable<Event> RSVPdEvents { get; set; }
+      //IEnumerable<Event> Attended { get; set; }
+      //EventsIEnumerable<Event> HostedEvents { get; set; }
+
+    }
+}

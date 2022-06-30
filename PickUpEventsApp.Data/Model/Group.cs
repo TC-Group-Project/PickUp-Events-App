1f96606e513d1d9a2cd8f5c56d11b0cc9bdd94a9
Jill Oakes <jericho_rules@hotmail.com>
2022-06-30 19:06:53 -0400

    Consolidate entities

--- PickUpEventsApp.Data/Model/Group.cs
+++ PickUpEventsApp.Data/Model/Group.cs
@@ -6,18 +6,16 @@ using System.Threading.Tasks;
 
 namespace PickUpEventsApp.Data.Model
 {
-    public class Event
+    public class Group
     {
         public User Owner { get; set; }
-        
-        public string Name { get; set; }
-        
-        public string Description { get; set; }
-        
-        public IEnumerable<User> Users { get; set; }
-        
-        public Location Location { get; set; }
 
-        public DateTime StartTime { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public IEnumerable<User> Users { get; set; }
+
+        public IEnumerable<Event> Events { get; set; }
     }
 }
